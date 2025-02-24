0b7c549b248d5e53a13690ecf9128fa78ed321ca
Leonty <krokxa228@gmail.com>
2025-02-24 13:34:30 +0500

    corret docker file

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import dotenv_values
 
 dotenv_values()
 
-MILVUS_HOST = os.getenv("MILVUS_HOST", "127.0.0.1")
+MILVUS_HOST = os.getenv("MILVUS_HOST", "standalone")
 MILVUS_PORT = os.getenv("MILVUS_PORT", "19530")
 
